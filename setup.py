6b3ca17d976eb626ba9d475fed281a6173a7fbd4
rushan7750 <shanrsjmax@gmail.com>
2020-10-10 10:15:44 +0530

    sixth commit

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
   download_url = 'https://github.com/rushan7750/regress/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['regress', 'simple', 'regrex'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'skikit-learn',
+          'scikit-learn',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
